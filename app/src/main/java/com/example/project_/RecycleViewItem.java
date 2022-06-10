8cdb67b730cca7283217aa7a4131e97bc1ae4a15
82102 <diiert33@gmail.com>
2022-06-11 01:00:48 +0900

    2줄 만들긴 했지만 어떻게 적용할지 모르겠음

---
+++ app/src/main/java/com/example/project_/RecycleViewItem.java
@@ -0,0 +1,57 @@
+package com.example.project_;
+
+public class RecycleViewItem {
+    // 상품 이미지, 상품 내용, 상품 가격
+    private String mImgName, mImgName2;
+    private String mMainText, mMainText2;
+    private String mSubText, mSubText2;
+
+    public String getImgName() {
+        return mImgName;
+    }
+
+    public void setImgName(String imgName) {
+        this.mImgName = imgName;
+    }
+
+    public String getMainText() {
+        return mMainText;
+    }
+
+    public void setMainText(String mainText) {
+        this.mMainText = mainText;
+    }
+
+    public String getSubText() {
+        return mSubText;
+    }
+
+    public void setSubText(String subText) {
+        this.mSubText = subText;
+    }
+
+
+    public String getImgName2() {
+        return mImgName2;
+    }
+
+    public void setImgName2(String imgName2) {
+        this.mImgName2 = imgName2;
+    }
+
+    public String getMainText2() {
+        return mMainText2;
+    }
+
+    public void setMainText2(String mainText2) {
+        this.mMainText2 = mainText2;
+    }
+
+    public String getSubText2() {
+        return mSubText2;
+    }
+
+    public void setSubText2(String subText2) {
+        this.mSubText2 = subText2;
+    }
+}
--- app/src/main/java/com/example/project_/main_menu.java
+++ app/src/main/java/com/example/project_/main_menu.java
@@ -3,21 +3,40 @@ package com.example.project_;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
-import android.app.Activity;
 
-public class main_menu extends Activity {
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+public class main_menu extends AppCompatActivity {
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 2;
+    private CircleIndicator3 mIndicator;
+    TextView userName;
+    private RecyclerView mRecyclerView;
+    private ArrayList<RecycleViewItem> mList;
+    private RecycleViewAdapter mRecyclerViewAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
-
         final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
+        // 소메뉴 스크롤
         findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +44,71 @@ public class main_menu extends Activity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        // 소메뉴 스와이핑
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
+
+
+        //ViewPager2
+        mPager = findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page, 0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        // 거의 무한대로 스와이핑 하도록 시작
+
+        mPager.setCurrentItem(1000); //시작 지점
+        mPager.setOffscreenPageLimit(4); //최대 이미지 수
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position % num_page);
+            }
+        });
+
+        firstInit();
+        String[] imageName = {"LG 울트라 HD TV (스탠드형)",
+                "LG 스탠바이미", "LG 올레드 evo 오브제컬렉션", "LG 올레드 TV (스탠드형)"};
+        String[] imagePrice = {"1,950,000원", "월 42,900원", "1,040,000원",
+                "9,900,000원", "1,890,000원"};
+
+        for (int i = 0; i < imageName.length; i++) {
+            addItem("iconName", imageName[i], imagePrice[i]);
+        }
+
+        mRecyclerViewAdapter = new RecycleViewAdapter(mList);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
     }
+
+    public void firstInit() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        mList = new ArrayList<>();
+    }
+
+    public void addItem(String imgName, String mainText, String subText) {
+        RecycleViewItem item = new RecycleViewItem();
+        item.setImgName(imgName);
+        item.setMainText(mainText);
+        item.setSubText(subText);
+        mList.add(item);
+    }
+
 }
